5b0a90365f6596129e077d21c1e5d8630a6aac36
VitoriaMagnus <vitoria.magnus@rede.ulbra.br>
2022-09-08 18:28:37 -0300

    first commit

---
+++ lib/components/EntradaTempo.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
+
+class EntradaTempo extends StatelessWidget {
+  final String title;
+  final int valor;
+  final void Function()? increment;
+  final void Function()? decrement;
+
+  const EntradaTempo({
+    Key? key,
+    required this.title,
+    required this.valor,
+    this.increment,
+    this.decrement,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final store = Provider.of<PomodoroStore>(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          this.title,
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: decrement,
+              child: Icon(
+                Icons.arrow_downward,
+                color: Colors.white,
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(15),
+                primary: store.estaTrabalhando() ? Colors.red : Colors.green,
+              ),
+            ),
+            Text(
+              '${this.valor} min',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: increment,
+              child: Icon(
+                Icons.arrow_upward,
+                color: Colors.white,
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(15),
+                primary: store.estaTrabalhando() ? Colors.red : Colors.green,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/pages/Pomodoro.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<PomodoroStore>(
+          create: (_) => PomodoroStore(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Pomodoro(),
+      ),
+    );
+  }
+}
+
+// import 'package:flutter_mobx/flutter_mobx.dart';
+// import 'package:mobx/mobx.dart';
+// import 'package:pomodoro/store/contador.store.dart';
+
+// final store = ContadorStore();
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Contador'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Observer(
+//               builder: (_) => Text(
+//                 '${store.contador}',
+//                 style: Theme.of(context).textTheme.headline4,
+//               ),
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 FloatingActionButton(
+//                   onPressed: store.incrementar,
+//                   tooltip: 'Increment',
+//                   child: const Icon(Icons.add),
+//                 ),
+//                 FloatingActionButton(
+//                   onPressed: store.decrementar,
+//                   tooltip: 'Increment',
+//                   child: const Icon(Icons.minimize),
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
